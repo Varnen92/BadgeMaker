aaf3c487ea78cf2efec7ca661acdd3e7baac743f
Robert Sandoval <robsand1992@yahoo.com>
2022-03-27 10:11:04 -0700

    updated employee badge to gather first name last name photo url and id

--- Program.cs
+++ Program.cs
@@ -5,36 +5,45 @@ namespace BadgeMaker
     class Program
     {
 
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine();
-                if (input == "")
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine();
+                if (firstName == "")
                 {
                     break;
                 }
+
+                // add a console readline for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
                 // Create a new Employee Instance
-                Employee currentEmployee = new Employee(input, "Smith");
-                employees.Add(currentEmployee.GetName());
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
             }
             return employees;
         }
 
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
 
         static void Main(string[] args)
         {
             // This is our employee getting code now
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
     }
